895bd8a8ce5ad2911aee8add207bea2a87bc73aa
Daniel Firth <daniel.firth@iohk.io>
2026-01-23 12:52:11 +0000

    Add hydra ci (#23)

--- flake.nix
+++ flake.nix
@@ -9,58 +9,24 @@
       flake = false;
     };
     hydra-coding-standards.url = "github:cardano-scaling/hydra-coding-standards/0.6.6";
+    import-tree.url = "github:vic/import-tree";
     nixpkgs.url = "github:nixos/nixpkgs";
   };
 
-  outputs = inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "aarch64-darwin"
-        "x86_64-linux"
-      ];
-      imports = [
-        inputs.hydra-coding-standards.flakeModule
-      ];
-      perSystem = { pkgs, ... }:
-        let
-          agdaPackages = pkgs.callPackage ./nix/initial-packages.nix {
-            inherit (pkgs.haskellPackages) Agda;
-            inherit (inputs) nixpkgs;
-          };
-          agdaLibraries = with agdaPackages;
-            [
-              abstract-set-theory
-              formal-ledger
-              standard-library
-              standard-library-classes
-              standard-library-meta
-            ];
-        in
-        rec {
-          coding.standards.hydra.enable = true;
-          packages = {
-            hydra-spec-pdf = agdaPackages.mkDerivation {
-              pname = "hydra-spec.pdf";
-              version = "0.0.1";
-              nativeBuildInputs = with pkgs; [
-                (agdaPackages.withPackages agdaLibraries)
-                (haskellPackages.ghcWithPackages (p: [ p.shake ]))
-                inkscape
-                texlive.combined.scheme-full
-              ];
-              meta = { };
-              src = ./.;
-              buildPhase = ''
-                shake
-              '';
-              installPhase = ''
-                mkdir $out
-                cp _build/hydra-spec.pdf $out/hydra-spec.pdf
-              '';
-            };
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./nix);
 
-            default = packages.hydra-spec-pdf;
-          };
-        };
-    };
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.iog.io"
+      "https://cardano-scaling.cachix.org"
+      "https://horizon.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+      "cardano-scaling.cachix.org-1:QNK4nFrowZ/aIJMCBsE35m+O70fV6eewsBNdQnCSMKA="
+      "horizon.cachix.org-1:MeEEDRhRZTgv/FFGCv3479/dmJDfJ82G6kfUDxMSAw0="
+    ];
+    allow-import-from-derivation = true;
+  };
+
 }
